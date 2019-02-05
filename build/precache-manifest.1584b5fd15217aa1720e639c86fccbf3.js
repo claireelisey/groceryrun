eed9fd5a4dfdeef27d346514c61d0951ff8da678
Claire Ouellette <claireelisey@gmail.com>
2019-02-04 22:25:48 -0600

    build folder

---
+++ build/precache-manifest.1584b5fd15217aa1720e639c86fccbf3.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "506c91645dbbae0a5f7e",
+    "url": "/static/js/main.506c9164.chunk.js"
+  },
+  {
+    "revision": "7661ad5324062b395e1f",
+    "url": "/static/js/1.7661ad53.chunk.js"
+  },
+  {
+    "revision": "ef5b5c7fb39a88c9d069e8dd58ffac54",
+    "url": "/index.html"
+  }
+];
